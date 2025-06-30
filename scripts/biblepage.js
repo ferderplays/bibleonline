05f3c75cde05ac64e9697a20d6359d29742f430a
ferderplays <86313430+ferderplays@users.noreply.github.com>
2025-06-30 22:13:13 +0200

    alpha 22

--- scripts/biblepage.js
+++ scripts/biblepage.js
@@ -135,3 +135,8 @@ $(".searchbutton").click(function() {
         });
     }
 });
+
+$(".verse-text").click(function() {
+    if (!$(this).hasClass("highlighted")) $(this).addClass("highlighted");
+    else $(this).removeClass("highlighted");
+});
